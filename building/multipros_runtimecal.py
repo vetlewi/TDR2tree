c08bcd1e06ffccd989e620fb5442fd8e688eb6ab
hannahberg <hannah.brg@gmail.com>
2019-02-13 15:05:14 +0100

    change core count

--- building/multipros_runtimecal.py
+++ building/multipros_runtimecal.py
@@ -1,13 +1,15 @@
-from multiprocessing import Pool
+afrom multiprocessing import Pool
 import os
 import numpy as np
 
 index = np.arange(84,131,1)
 def timesort(i):
-    command = './TDR2tree -i ../../PR271A/R'+ str(i)+'_* -o ../output_files/r'+str(i)+'time.root'
-    # command = './TDR2tree -i /media/hannahcb/PR271/PR271A/R'+ str(i)+'_* -o ../output_files/r'+str(i)+'time.root'
+    # command = './TDR2tree -i ../../PR271A/R'+ str(i)+'_* -o ../output_files/r'+str(i)+'time.root'
+    command = './TDR2tree -i /media/hannahcb/PR271/PR271A/R'+ str(i)+'_* -o ../output_files/r'+str(i)+'time.root'
+    print(i,'\n')
     os.system(command)
 
+
 if __name__ == '__main__':
-    pool = Pool(int(os.cpu_count()/2))                         # Create a multiprocessing Pool
+    pool = Pool(os.cpu_count())                         # Create a multiprocessing Pool
     pool.map(timesort, index)
