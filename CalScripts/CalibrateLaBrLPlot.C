cb1b723ba8bab7d7122f3eeffdaa48eeec077b77
hannahberg <hannah.brg@gmail.com>
2019-02-13 12:41:27 +0100

    plotting of time calibration

---
+++ CalScripts/CalibrateLaBrLPlot.C
@@ -0,0 +1,54 @@
+
+void CalibrateTimePlot(const char *rname)
+{
+	TFile *file = TFile::Open(rname);
+
+	TH2 *m = (TH2 *)file->Get("time_labrS");
+
+	TSpectrum spec;
+
+	char tmp[1024];
+
+	double peakPos[1024];
+
+	const int num_labrL = 6;
+
+	for (int i = 0 ; i < num_labrL ; ++i ){
+		sprintf(tmp, "px_%d", i);
+		TH1 *h = m->ProjectionX(tmp, i+1, i+1);
+		//h->Rebin(5);
+		h->Draw();
+		spec.Search(h);
+		int n_peaks = spec.GetNPeaks();
+		cout << i << ": " << n_peaks << endl;
+
+		double param[3] = {200., spec.GetPositionX()[0], 1.0};
+		TF1 *fit = new TF1("total","gaus(0)",spec.GetPositionX()[0]-1.0,spec.GetPositionX()[0]+1.0);
+
+		for (Int_t k=0; k<3; k++) {
+        fit->SetParameter(k, param[k]);
+    }
+
+		h->Fit("total", "bR");
+		// for nice plotting
+		h->SetTitle("");
+		h->GetYaxis()->SetTitle("Counts/0.1 ns");
+		h->GetYaxis()->SetTitleSize(0.05);
+		h->GetXaxis()->SetTitleSize(0.05);
+		h->GetXaxis()->SetLabelSize(0.045);
+   		h->GetYaxis()->SetLabelSize(0.045);
+
+		peakPos[i] = fit->GetParameter(1);
+		fit->Draw("same");
+
+	}
+	printf("shift_t_labrS =\t");
+	for (int i = 0 ; i < num_labrL ; ++i){
+		printf("%2.6f\t", -peakPos[i]);
+	}
+
+	cout << endl;
+
+	//file->Close();
+
+}
