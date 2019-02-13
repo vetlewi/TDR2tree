2e19e38c5767faf218b86135789006729aea8d2a
hannahberg <hannah.brg@gmail.com>
2019-02-13 15:12:40 +0100

    change core count

--- building/multipros_runtimecal.py
+++ building/multipros_runtimecal.py
@@ -6,7 +6,7 @@ index = np.arange(84,131,1)
 def timesort(i):
     command = './TDR2tree -i ../../PR271A/R'+ str(i)+'_* -o ../output_files/r'+str(i)+'time.root'
     # command = './TDR2tree -i /media/hannahcb/PR271/PR271A/R'+ str(i)+'_* -o ../output_files/r'+str(i)+'time.root'
-    print(i,'\n')
+    print(i)
     os.system(command)
 
 
