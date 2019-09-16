757fb05d0a5dbe1d9fa69238ab1457562fc437c9
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2019-09-16 13:29:51 +0200

    Added a configuration structure to reduce the number of parameters needed to be passed around.

---
+++ Configuration/Options.h
@@ -0,0 +1,19 @@
+//
+// Created by Vetle Wegner Ingeberg on 16/09/2019.
+//
+
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+struct Options
+{
+    char *output_file;
+    bool buildTree;
+    bool addback;
+    char *tree_name;
+    char *tree_title;
+    int coins_time;
+};
+
+
+#endif // OPTIONS_H
