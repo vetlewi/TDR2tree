4a1e705a10827922eb287a2ff6643cee368b18cc
vetlewi <v.w.ingeberg@fys.uio.no>
2022-03-07 12:10:17 +0100

    Added a timestamp raw field to keep the uncalibrated timestamp for later use.

--- include/Containers/BasicStruct.h
+++ include/Containers/BasicStruct.h
@@ -11,9 +11,10 @@ struct word_t {
     bool finishcode;        //!< Pile-up flag.
     bool veto;              //!< Veto flag
     int64_t timestamp;		//!< Timestamp in [ns].
+    int64_t timestamp_raw;  //!< Raw timestamp in [ns]. Before any alignment
     double energy;          //!< Calibrated energy [keV]
     bool cfdfail;           //!< Flag to tell if the CFD was forced or not.
-    double cfdcorr;         //!< Correction from the CFD [ns].
+    double cfdcorr;         //!< Correction from the CFD [ns]. Before any alignment
 };
 
 #endif // BASICSTRUCT_H
